8facd474dfbe3a71d74f8df6c9e31317deb67ee5
jwp36 <jwp36@drexel.edu>
2015-02-01 16:14:47 -0500

    Initial commit of project files, source files, and anagram dictionary.

---
+++ anagram_solver/anagram_solver.c
@@ -0,0 +1,14 @@
+// anagram_solver.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    // Allow memory-leak checks to occur at each exit point in the program.
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	return 0;
+}
+
