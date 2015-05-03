2027e5270e5f2e0c0e0d6fadf327b08c81ae8b44
jwp36 <jwp36@drexel.edu>
2015-05-03 18:35:08 -0400

    Updated solve() and has_unsupported_chars() methods.
    Updated "has_unsupported_chars" method so that only non-diacritic
    characters are supported.
    
    "solve" method has been reorganized; code that used to be in the method is
    now in the "are_anagrams" method.
    
    Added a static string to be printed when the create method fails.

--- anagram_solver/anagram_solver.c
+++ anagram_solver/anagram_solver.c
@@ -1,51 +1,81 @@
-// Fill this in sometime.
-
 #include "stdafx.h"
 
-#define WORD_BUFFER_SIZE 128
 static const int MAX_WORD_LEN = 64;
 static const int MIN_WORD_LEN = 2;
+
+static const char ERROR_UNABLE_TO_OPEN_FILE[] = "ERROR: Unable to open input file: %s\n";
 
 int cmpfunc(const void * a, const void * b)
 {
     return (*(char*)a - *(char*)b);
 }
 
-//char test[32] = "asdf";
-
-//qsort(test, 4, sizeof(char), cmpfunc);
-
-//printf("%s", test);
-
-
-//Determines if a word has an unsupported characters.
-//Assumes that the word ends with a newline.
-bool_t has_unsupported_chars(char* word)
+errno_t anagram_solver_create(hash_table_t** hash_table_addr, char* input_file_name)
 {
-    int ind = 0;
-    unsigned char c = word[ind];
+    //Open the input file for reading
+    FILE* input_file;
+    errno_t error;
+    error = fopen_s(&input_file, input_file_name, "r");
 
-    while (c != '\n')
+    if (error != 0)
     {
-        //TODO: This needs to be changed.
-        //Supported characters are letters and spaces only. 
-
-
-        //Supported characters exist between 0x20 and 0x7E.
-        //The apostrophe, 0x27, is not supported. 
-        if ((c < 0x20) | (c > 0x7E) | (c == 0x27))
-        {
-            return true;
-        }
-        
-        ind++;
-        c = word[ind];
+        fprintf(stderr, ERROR_UNABLE_TO_OPEN_FILE, input_file_name);
+        return error;
     }
 
-    return false;
+    //Create an empty hash table
+    hash_table_t* hash_table = hash_table_create();
+
+    if (hash_table == NULL)
+    {
+        return -1;
+    }
+
+    //Insert valid words from the input file into the hash table
+    char word_buffer[WORD_BUFFER_SIZE];
+    while (fgets(word_buffer, WORD_BUFFER_SIZE, input_file))
+    {
+        if (is_supported(word_buffer))
+        {
+            //Strip the newline at the end of the word
+            int word_len = strlen(word_buffer);
+            word_buffer[word_len - 1] = '\0';
+            word_len--;
+
+            char* word = malloc(sizeof(char) * word_len + 1);
+
+            if (word == NULL)
+            {
+                return -1;
+            }
+            else
+            {
+                //Copy contents of the word buffer into word
+                if (strcpy_s(word, word_len + 1, word_buffer) != 0)
+                {
+                    return -1;
+                }
+
+                //Insert the word into the hash table
+                if (hash_table_insert_word(word, hash_table) == NULL)
+                {
+                    return -1;
+                }
+            }
+        }
+    }
+
+    *hash_table_addr = hash_table;
+    fclose(input_file);
+    return 0;
 }
 
-//Determines if a word is supported.
+bool_t anagram_solver_word_is_supported(char* word)
+{
+    return is_supported(word);
+}
+
+// Determine if a word is supported.
 bool_t is_supported(char* word)
 {
     int len = strlen(word);
@@ -66,89 +96,39 @@ bool_t is_supported(char* word)
 
     //Argument must adhere to the min and max word lengths.
     //MIN_WORD_LEN is increased by 1 compensate for the newline character.
-    bool_t length_verified = (len >= MIN_WORD_LEN + 1) & (len <= MAX_WORD_LEN);
+    bool_t length_verified = (len >= MIN_WORD_LEN + 1) && (len <= MAX_WORD_LEN);
 
     //Argument must end in a newline.
     char last = word[len - 1];
     bool_t last_char_verified = (last == '\n');
-    
-    return newline_count_verified & length_verified & last_char_verified & !has_unsupported_chars(word);
+
+    return newline_count_verified && length_verified && last_char_verified && !has_unsupported_chars(word);
 }
 
-errno_t anagram_solver_create(hash_table_t** hash_table_addr, char* input_file_name)
+// Determine if a word has an unsupported characters. Assumes that the word ends with a newline.
+// Only letters without diacritical marks are supported characters.
+bool_t has_unsupported_chars(char* word)
 {
-    FILE* input_file;
+    int ind = 0;
+    unsigned char c = word[ind];
 
-    errno_t error;
-    error = fopen_s(&input_file, input_file_name, "r");
-
-    //how do I do my error checking properly?
-    if (error != 0)
+    while (c != '\n')
     {
-        fprintf(stderr, "Can't open input file: %s\n", input_file_name);
-        return error;
-    }
-
-    hash_table_t* hash_table = hash_table_create();
-
-    //needs to be errno_t
-    if (hash_table == NULL)
-    {
-        return -1;
-    }
-
-    char word_buffer[WORD_BUFFER_SIZE];
-    while (fgets(word_buffer, WORD_BUFFER_SIZE, input_file))
-    {
-        if (is_supported(word_buffer))
+        //If c is not a letter...
+        if (!(((c >= 0x41) && (c <= 0x5A)) || ((c >= 0x61) && (c <= 0x7A))))
         {
-            //Strip the newline at the end of the word
-            int word_len = strlen(word_buffer);
-            word_buffer[word_len - 1] = '\0';
-            word_len--;
-
-            char* word = malloc(sizeof(char) * word_len + 1);
-
-            if (word == NULL)
-            {
-                return -1;
-            }
-            else
-            {
-                error = strcpy_s(word, word_len + 1, word_buffer); 
-
-                if (error != 0)
-                {
-                    fprintf(stderr, "Failed copying string.: %s\n", word_buffer);
-                    return -1;
-                }
-               
-                //This needs to return errno instead of NULL.
-                void* error2 = hash_table_insert_word(word, hash_table);
-
-                if (error2 == NULL)
-                {
-                    fprintf(stderr, "Failed creating hash table.: %s\n", word_buffer);
-                    return -1;
-                }
-            }
+            return true;
         }
+
+        ind++;
+        c = word[ind];
     }
-    
-    *hash_table_addr = hash_table;
-    fclose(input_file);
-    return 0;
+
+    return false;
 }
 
-//Checks if word is an anagram of sorted_word. 
-bool_t is_anagrams(char* sorted_control_word, char* word)
-{
-    int word_len = strlen(word);
-    qsort(word, word_len, sizeof(char), cmpfunc);
-
-    return strcmp(sorted_control_word, word) == 0;
-}
-
+//TODO: This funcion should return a list of anagrams. The list may be empty. 
+// The function invoking this function can then render the anagrams arbitrarily.
 void anagram_solver_solve(hash_table_t* hash_table, char* control_word)
 {
     unsigned hash = hash_table_hash(control_word);
@@ -157,74 +137,52 @@ void anagram_solver_solve(hash_table_t* hash_table, char* control_word)
     {
         hash_node_t* hash_node = hash_table->table + hash;
 
-
-        //char x[] = "cat";
-        //char y[] = "Cat";
-
-        //unsigned xh = hash_table_hash(x);
-        //unsigned yh = hash_table_hash(y);
-
-        ////untf("%s: %d\n%s:%d", x, xh, y, yh);
-
-
         //Make a copy of the argument
         int control_word_len = strlen(control_word);
         char* sorted_control_word = strdup(control_word);
-        
-        //set to lowercase..
-        for (int i = 0; i < control_word_len + 1; i++)
+
+        //Set all chars to lowercase and sort the word
+        for (int i = 0; i < control_word_len; i++)
         {
             sorted_control_word[i] = tolower(sorted_control_word[i]);
         }
-
         qsort(sorted_control_word, control_word_len, sizeof(char), cmpfunc);
 
-        
-
-        //printf("Control word is %s\n", control_word);
-        //printf("Sorted control word is %s\n", sorted_control_word);
-
-        
+        //Search the linked list corresponding to the current hash for anagrams
         linked_list_node_t* curr = hash_node->head;
         while (curr != NULL)
         {
-            //make a copy of the linked list word so we can keep the original after it gets sorted...
-            char* ll_word = strdup(curr->word);
-
-
-            //printf("Control word is %s\n", ll_word);
-
-            for (int i = 0; i < curr->len + 1; i++)
-            {
-                ll_word[i] = tolower(ll_word[i]);
-            }
-            
-            //printf("Sorted control word is %s\n", ll_word);
-
-
-
-            //if ((curr->len == strlen(control_word)) & (is_anagrams(sorted_control_word, ll_word)))
-            //{
-            //    printf("%s\n", curr->word);
-            //}
-
+            //If the length of the word in the linked list doesn't match the length of the control word, immediately move to the next node.
             if (curr->len != control_word_len)
                 goto next;
 
-            if (is_anagrams(sorted_control_word, ll_word))
+            //Create a copy of the word in the linked list so that it doesn't get destroyed by sorting inside are_anagrams.
+            char* ll_word = strdup(curr->word);
+
+            if (are_anagrams(sorted_control_word, ll_word))
             {
-                printf("%s\n", curr->word);
+                printf("\t%s\n", curr->word);
             }
 
             next:
                 curr = curr->next;
-                free(ll_word);
         }
 
         free(sorted_control_word);
     }
-    else
-    {
-        printf("Hash table doesn't contain any anagrams for that word.");
-    }
 }
+
+// Determine if word is an anagram of sorted_control_word. 
+bool_t are_anagrams(char* sorted_control_word, char* word)
+{
+    //Set all chars to lowercase and sort the word
+    int word_len = strlen(word);
+    for (int i = 0; i < word_len; i++)
+    {
+        word[i] = tolower(word[i]);
+    }
+    qsort(word, word_len, sizeof(char), cmpfunc);
+
+    return strcmp(sorted_control_word, word) == 0;
+}
+
